e4aa4610d8646a29dd4b07fe43352a76ace35e5f
grossmj <grossmj@gns3.net>
2024-08-11 15:14:48 -0600

    Download cloud image with CURL

--- build_base_vm.sh
+++ build_base_vm.sh
@@ -7,6 +7,7 @@ export GNS3VM_VERSION=`cat version`
 echo "Build Base VM $GNS3VM_VERSION for AMD64"
 
 rm -Rf output-qemu-amd64
+curl -O https://cloud-images.ubuntu.com/noble/current/noble-server-cloudimg-amd64.img  # download the cloud image outside packer
 packer build -only=qemu-amd64 $* base_vm.json
 
 cd output-qemu-amd64
