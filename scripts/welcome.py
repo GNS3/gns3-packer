1929fae0d44f63ab1bec1bbc4b63872ddc51a76f
grossmj <grossmj@gns3.net>
2024-10-05 22:00:37 +0700

    Update welcome.py

--- scripts/welcome.py
+++ scripts/welcome.py
@@ -294,7 +294,7 @@ Uptime: {uptime}\n\n""".format(
     if ip:
         content += "IP: {ip} PORT: {server_port}\n\nTo log in using SSH: ssh gns3@{ip}\nPassword: gns3\n\nTo launch the Web-Ui: {protocol}://{ip}{url_port} (default username/password is admin/admin)\n\nImages and projects are stored in '/opt/gns3'""".format(protocol=protocol, ip=ip, server_port=server_port, url_port=port_string)
     else:
-        content += "eth0 is not configured. Please manually configure by selecting the 'Network' entry in the menu."
+        content += "eth0 is not configured (no IP address found). Please manually configure by selecting the 'Network' entry in the menu."
 
     try:
         d.msgbox(content)
